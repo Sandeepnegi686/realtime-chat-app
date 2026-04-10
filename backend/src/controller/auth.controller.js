52851d340cc22ec3f40ab09bed714e100cc1e51e
Sandeep Negi <39149906+Sandeepnegi686@users.noreply.github.com>
2026-04-10 21:54:30 +0530

    working

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,5 +1,5 @@
 import userModel from "../models/user.model.js";
-
+import bcrypt from "bcryptjs";
 export const signUp = async (req, res) => {
   try {
     const { fullName, email, password } = req.body;
@@ -9,6 +9,8 @@ export const signUp = async (req, res) => {
         .json({ success: false, message: "Please write all fields" });
     }
     const user = await UserModel.create(req.body);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt;
   } catch (error) {
     return res
       .status(500)
